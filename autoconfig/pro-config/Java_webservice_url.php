bd5bf535ec22720f4f9d15855fb7e118621cfda2
jiangcai <jiangcai@milanoo.com>
2015-05-18 10:58:54 +0800

    ma

--- autoconfig/pro-config/Java_webservice_url.php
+++
@@ -1,3 +0,0 @@
-<?php
-namespace config;
-if (!defined('JAVA_WEBSERVICE_URL')) define('JAVA_WEBSERVICE_URL', 'http://10.20.20.11:81/');//JAVA
--- config/Java_webservice_url.php
+++
@@ -1,3 +0,0 @@
-<?php
-namespace config;
-if (!defined('JAVA_WEBSERVICE_URL')) define('JAVA_WEBSERVICE_URL', 'http://192.168.11.16:8080/');//JAVA
