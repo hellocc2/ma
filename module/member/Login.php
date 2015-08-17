70effd826cb4991fd844c9c0da9ec6e2e79d2cb9
jiangcai <jiangcai@milanoo.com>
2015-08-17 17:28:39 +0800

    ma

--- module/member/Login.php
+++ module/member/Login.php
@@ -12,26 +12,28 @@ class Login extends \Lib\common\Application {
 		$tpl = \Lib\common\Template::getSmarty ();
 		$username = R::getParams ('username');
 		$password = R::getParams ('password');
-		
+		//var_dump(R::getParams());exit;
 		if (!empty ($username)) {
 
 			$db = \Lib\common\Db::get_db ();
 			$password=md5($password);
 
 			if ($username&&$password) {
- 				$sql = "SELECT * FROM `rmb_money_member` m WHERE username='".$username."' AND password='".$password."'";
- 				$row = $db->getrow ( $sql );
+ 				//$sql = "SELECT * FROM `rmb_money_member` WHERE member_name='".$username."' AND member_password='".$password."'";
+				$sql="SELECT * FROM mecoo_bar_label";
+ 				$row = $db->getRow ($sql);
+				echo '<pre/>';var_dump($row);exit;
  				if (empty($row)){
- 					$tpl->assign ( 'error', '用户名密码验证成功，但是你没有查看 MA 的权限请找相关人员开通' );
+ 					$tpl->assign ( 'error', '登录失败，请重新登陆' );
  					$tpl->display ( 'member_login.htm' );
 					exit ();
  				}
 				
-				$_SESSION [SESSION_PREFIX . "MemberId"] = $row['uid'];
+				$_SESSION [SESSION_PREFIX . "MemberId"] = $row['id'];
 				header ( "Location: index.php" );
 				exit;
 			} else {
-				$tpl->assign ( 'error', '登录失败，请使用米兰账号登陆' );
+				$tpl->assign ( 'error', '登录失败，请重新登陆' );
 				$tpl->display ( 'member_login.htm' );
 				exit ();
 			}
